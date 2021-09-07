409c88aa1fb3fb8aef67e5018c39a5f021013dd7
Jenny Lin <misslinyang91@gmail.com>
2021-09-07 15:53:27 +1000

    created the game loop

--- app.js
+++ app.js
@@ -4,3 +4,25 @@ const canvasContext = canvasEl.getContext("2d");
 
 canvasEl.height = 600;
 canvasEl.width = 800;
+
+let xP = 400;
+let yP = 300;
+let radius = 50;
+let speed = 10;
+
+//running the game -> the game loop
+function runGame() {
+  requestAnimationFrame(runGame);
+
+  drawBall();
+}
+
+//drawing the ball
+function drawBall() {
+  canvasContext.fillStyle = "white";
+  canvasContext.beginPath();
+  canvasContext.arc(xP, yP, radius, 0, Math.PI * 2);
+  canvasContext.fill();
+}
+
+runGame();
